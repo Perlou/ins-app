dc88ef9ea8f9ca5678030d980914ad545c47b888
Perlou <perloukevin@gmail.com>
2023-03-01 12:56:21 +0800

    feat: 接入swagger

--- server/src/configuration.ts
+++ server/src/configuration.ts
@@ -2,14 +2,29 @@ import { App, Configuration, ILifeCycle } from '@midwayjs/core'
 import { Application } from 'egg'
 import { join } from 'path'
 import * as egg from '@midwayjs/web'
+import * as swagger from '@midwayjs/swagger'
 
 @Configuration({
-    imports: [egg],
+    imports: [
+        egg,
+        {
+            component: swagger,
+            enabledEnvironment: ['local'],
+        },
+    ],
     importConfigs: [join(__dirname, './config')],
 })
 export class ContainerLifeCycle implements ILifeCycle {
     @App()
     app: Application
 
-    async onReady() {}
+    async onReady() {
+        console.log('====================================')
+        console.log('🚀  Your awesome APP is launching...')
+        console.log('====================================')
+
+        console.log('====================================')
+        console.log('✅  Your awesome APP launched')
+        console.log('====================================')
+    }
 }
